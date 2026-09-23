b4a14c9a205b3b9310394cc286029f9e84ccc164
Youza Ahsan <youzaahsan1@gmail.com>
2026-09-23 21:24:06 +0500

    Update contact.php

--- patient/contact.php
+++ patient/contact.php
@@ -96,7 +96,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <div class="col-md-6">
                <div class="map-responsive">
-                  <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France" width="600" height="540" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
+                  <iframe src="https://www.google.com/maps/embed/v1/place?key=&amp;q=Eiffel+Tower+Paris+France" width="600" height="540" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
                </div>
             </div>
          </div>
